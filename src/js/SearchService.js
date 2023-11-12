a974dd03dd8e2eab77206fecab038acd114ec36f
Oleksandrdz <134951160+Oleksandrdz@users.noreply.github.com>
2023-11-12 15:40:41 +0100

    j

--- src/js/SearchService.js
+++ src/js/SearchService.js
@@ -1,8 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export { SearchService, Notify };
 import axios from "axios";
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '40630400-52ca2fa072115e17b4bb97016';
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "40630400-52ca2fa072115e17b4bb97016";
 
 async function SearchService(currentPage, searchQuery) {
     const parameters = new URLSearchParams({
